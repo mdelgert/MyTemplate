d90629ef137138bca4dad0f83336db574eaaf2de
Matthew David Elgert <mdelgert@yahoo.com>
2024-10-01 13:21:47 -0400

    Add darktheme to Swashbuckle

--- MyTemplate.WebAPI/Program.cs
+++ MyTemplate.WebAPI/Program.cs
@@ -1,34 +1,34 @@
+namespace MyTemplate.WebAPI;
 
-namespace MyTemplate.WebAPI
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer(); 
+        builder.Services.AddSwaggerGen(c =>
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            c.SwaggerDoc("v1", new OpenApiInfo
             {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+                Title = "WebAPI",
+                Version = "v1"
+            });
+        });
 
-            app.UseAuthorization();
+        var app = builder.Build();
 
-
-            app.MapControllers();
-
-            app.Run();
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(ModernStyle.Dark);
         }
+
+        app.UseAuthorization();
+        app.MapControllers();
+        app.Run();
     }
 }
